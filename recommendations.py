c7423740f49737fd87014123dad4c1b0d4b41649
Zilraen <zilraen@gmail.com>
2014-07-14 18:16:44 +0300

    + added sim_distance function (p30)

--- recommendations.py
+++ recommendations.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 critics={
 'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
 'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
@@ -7,3 +9,21 @@ critics={
 'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}
 }
+
+# Возвращает оценку подобия person1 и person2 на основе расстояния
+def sim_distance(prefs, person1, person2):
+    # Получить список предметов, оцененных обоими
+    si={}
+
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item]=1
+
+    # Если нет ни одной общей оценки, вернуть 0
+    if len(si)==0:
+        return 0
+
+    # Сложить квадраты разностей
+    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) for item in prefs[person1] if item in prefs[person2]])
+    
+    return 1/(1+sum_of_squares)
